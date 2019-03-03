8bbd624dd07013019974b76a28efc23c5c17f5d3
Kelang <Kelang@users.noreply.github.com>
2019-03-03 02:44:09 -0500

    Formatage de la page d'un projet

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,52 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" type="image/png" href="media/favicon.ico"/>
-
-    
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    
-       <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    
- 
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> 
     <script src="{{ asset('js/app.js') }}" defer></script>
     <title>@yield('title', 'Kevin Langlois')</title>
-    
-
 </head>
 
 <body>
+    <div id="BurgerMenuWrapper">
+            <div class="BurgerMenu menu">
+                <input type="checkbox" class="checkBurger">
+                <div class="BurgerMenuLine line1"></div>
+                <div class="BurgerMenuLine line2"></div>
+                <div class="BurgerMenuLine line3"></div>
+            </div>
+        </div>
     
-        <div id="BurgerMenuWrapper">
-                <div class="BurgerMenu menu">
-                    <input type="checkbox" class="checkBurger">
-                    <div class="BurgerMenuLine line1"></div>
-                    <div class="BurgerMenuLine line2"></div>
-                    <div class="BurgerMenuLine line3"></div>
-                </div>
+        <div id="side-menu" class="MenuContainer ">
+            <div>
+            <nav class="navContainer">
+                <a class="navLink souligner" href="/">Accueil</a>
+                <a class="navLink souligner" href="/projets">Projets</a>
+                <a class="navLink souligner" href="/apropos">À propos</a>
+                <a class="navLink souligner" href="/contact">Contact</a>
+            </nav>
             </div>
-        
-        
-            <div id="side-menu" class="MenuContainer ">
-                <div>
-                <nav class="navContainer">
-                    <a class="navLink souligner" href="/">Accueil</a>
-                    <a class="navLink souligner" href="/projets">Projets</a>
-                    <a class="navLink souligner" href="/apropos">À propos</a>
-                    <a class="navLink souligner" href="/contact">Contact</a>
-                </nav>
-                </div>
-                <div class="menuSocial">
-                    <ul class="social">
-                        <li class="socialItem"><a href="https://www.linkedin.com/in/k-langlois"><i class="fab fa-linkedin"></i></a></li>
-                        <li class="socialItem"><a href="https://github.com/Kelang"><i class="fab fa-github"></i></a></li>
-                    </ul>
-                </div>
-        
+            <div class="menuSocial">
+                <ul class="social">
+                    <li class="socialItem"><a href="https://www.linkedin.com/in/k-langlois"><i class="fab fa-linkedin"></i></a></li>
+                    <li class="socialItem"><a href="https://github.com/Kelang"><i class="fab fa-github"></i></a></li>
+                </ul>
             </div>
-
-
+    
+        </div>
 
     @yield('content')
     <footer>
--- resources/views/projets/show.blade.php
+++ resources/views/projets/show.blade.php
@@ -4,13 +4,13 @@
 <main>
 
         <h1 data-aos="fade-down">{{$projet->titre}}</h1>
-        <div data-aos="fade-up" class="technologies">
+        <div data-aos="fade-right" class="technologies">
             @foreach($technologies as $technologie)
             <span>{{$technologie}}</span>
             @endforeach
         </div>
        <div class="topImg">
-            <img src="media/projets/{{$projet->id}}.png" alt="">
+            <img data-aos="fade-up" src="/media/projets/{{$projet->id}}.png" alt="">
        </div>
        <h2 data-aos="fade-up">Collaborateurs</h2>
        <div data-aos="fade-up" class="collaborateurs">
@@ -23,27 +23,25 @@
 
 
             </ul>
-     </div>
+        </div>
     
-
-
        <div data-aos="fade-right" class="description">
        <p>{{$projet->descriptionPage}}</p>
        </div>
         
        <div class="galerie">
-
-        
-        @foreach($images as $image)
-        
-       <img data-aos="fade-up" src="/{{$image->chemin}}" alt="{{$image->alt}}">
-        @endforeach
-       </div>
-
- 
-
-     
-
-       
-
+        @if($images->count())
+            @foreach($images as $image)
+                @if($image->type == 'image')
+                <img data-aos="fade-up" src="/{{$image->chemin}}" alt="{{$image->alt}}">
+                @elseif($image->type == 'video')
+                <video controls>
+                    <source src="/{{$image->chemin}}" type="video/mp4">
+                    Votre navigateur ne supporte pas la lecture video.
+                </video>
+                @endif
+            @endforeach
+            </div>
+       @endif
+</main>
 @endsection
